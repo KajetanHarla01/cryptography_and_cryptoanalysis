59ceaabcc63815cd06ebf2222af93058018dec0a
KajetanHarla01 <kajetanharla01@gmail.com>
2024-03-15 21:05:06 +0100

    Tutoral1 exercises

--- Tutorial1/main.py
+++ Tutorial1/main.py
@@ -1,21 +1,18 @@
+from sympy import *
 def factorial(n):
     if n == 0:
         return 1
     else:
         return n*factorial(n-1)
 
-def pascal_triangle(n, tab = []):
-    if len(tab) == n:
-        return
-    temp = []
-    temp.append(1)
-    for i in range(0, len(tab) - 1):
-        temp.append(tab[i] + tab[i+1])
-    for element in temp:
-        print(str(element) + " ")
-    #print("\n")
-    print(len(temp))
-    return pascal_triangle(n, temp)    
+def generate_pascal_triangle(n):
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+    return triangle   
 
 
 option = int(input("Check option number (1 - 6): "))
@@ -44,6 +41,31 @@ elif option == 3:
 elif option == 4:
     print("4) Print out Pascal's triangle.")
     layers = int(input("n = "))
-    pascal_triangle(layers)
+    pascal_triangle = generate_pascal_triangle(layers)
+    for row in pascal_triangle:
+        print(row)
+elif option == 5:
+    print("5) Use the sieve of Eratosthenes to find all primes less than 10000.")
+    n = 10000
+    is_prime = [True] * n
+    is_prime[0] = is_prime[1] = False
+
+    for i in range(2, int(n**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i*i, n, i):
+                is_prime[j] = False
+
+    primes = [i for i in range(n) if is_prime[i]]
+    print(primes)
+elif option == 6:
+    print("6) Verify Goldbach’s conjecture for all even integers less than 10000.")
+    n = 10000
+    for i in range(4, n, 2):
+        firstNum = prevprime(i)
+        secNum = i - firstNum
+        while not isprime(secNum):
+            firstNum = prevprime(firstNum)
+            secNum = i - firstNum
+        print(f"{i} = {firstNum} + {secNum}")
 else:
     print("Incorrect option number.")
