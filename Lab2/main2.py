c22f270755d493f4c1a3d6e0b6cab03dfceffa30
KajetanHarla01 <kajetanharla01@gmail.com>
2024-03-25 00:49:28 +0100

    Some fixes - lab2

--- Lab2/main2.py
+++ Lab2/main2.py
@@ -1,46 +1,97 @@
 import random
 import sympy
-from sympy.solvers import solve
-from sympy import Symbol
-s = 328 #int(input("Input s: "))
-n = 4 #int(input("Input n: "))
-t = 3 #int(input("Input t: "))
+
+def format_result(res):
+    return "{:.2f}".format(res)
+
+print("----------------------------------")
+print("Parameters setting")
+s = int(input("Input secret - s: "))
+n = int(input("Input shares count - n: "))
+t = int(input("Input required shares count - t: "))
 sn = []
 a = []
-
 a.append(s)
-min_p = (s if s > n else n)  + 1
-prime_num = random.randint(0, 20)
-p = sympy.nextprime(min_p)
-for i in range(prime_num):
-    p = sympy.nextprime(min_p)
-    min_p = p
 
-print("p = " + str(p))
+p = int(input("Input prime number - P (or 0 to generate it randomly): "))
+
+if p == 0:
+    min_p = (s if s > n else n)  + 1
+    prime_num = random.randint(0, 20)
+    p = sympy.nextprime(min_p)
+    for i in range(prime_num):
+        p = sympy.nextprime(min_p)
+        min_p = p
+
 for i in range(t - 1):
     a.append(random.randint(1, 1000))
 
+print("P = " + str(p))
 print("a = " + str(a))
+print("Calculated polynomial: ")
+out = "f(x) = "
+for i, an in enumerate(a[::-1]):
+    index = len(a) - 1 - i
+    if index == 1:
+        out += str(an) + "x + "
+    elif index != 0:
+        out += str(an) + "x^" + str(index) + " + "
+    else:
+        out += str(an)
+print(out)
 
-for i in range(4):
+print("----------------------------------")
+print("Calculating shares")
+for i in range(n):
     sum = 0
     for j, an in enumerate(a):
         sum += pow((i+1), j) * an
     sn.append(sum % p)
 
-print("sn = " + str(sn))
+for i, key in enumerate(sn):
+    print("s" + str(i + 1) + " = f(" + str(i) + ") mod " + str(p) + " = " + str(key))
 
-decrypt_secrets = {0,1,3} #[]
+shares = []
+for i, si in enumerate(sn):
+    share = {"x" : (i + 1), "y" : si}
+    shares.append(share)
 
-#for i in range(t):
-#    sec = int(input("Secret number to decrypt: "))
-#    decrypt_secrets.append(sec)
+print("Coordinates: ")
+for i, share in enumerate(shares):
+    print(f"(x{i},y{i}) = ({share['x']},{share['y']})")
 
-symbols = []
-for sec in decrypt_secrets:
-    symbols.append(Symbol("a" + str(sec)))
-
-equations = []
+print("----------------------------------")
+print("Secret recovery")
+print("Select secrets: ")
+decrypt_secrets = []
+l = []
 for i in range(t):
-    for a in symbols:
-        sympy.Eq()
+    sec = int(input())
+    decrypt_secrets.append(sec)
+
+for coordinate in decrypt_secrets:
+    denom = 1
+    num = 1
+    xn = shares[coordinate - 1]["x"]
+    for share in shares:
+        if share["x"] in decrypt_secrets and share["x"] != coordinate:
+            num *= -share["x"]
+            denom *= (xn - share["x"])
+    l.append(num / denom)
+
+print("Free terms of calculated polynomials ln: ")
+for n, ln in enumerate(l):
+    print(f"l{n} = {format_result(ln)}")
+
+print("Calculating the secret: ")
+out = "s = ("
+sum = 0
+for n, ln in enumerate(l):
+    result = sympy.Mod(ln*shares[decrypt_secrets[n] - 1]['y'], p)
+    print(f"({shares[decrypt_secrets[n] - 1]['y']} * {format_result(ln)}) mod {p} = {format_result(result)}")
+    sum += result
+    out += str(format_result(result))
+    if n != len(l) - 1:
+        out += " + "
+out += f") mod {p} = {format_result(sympy.Mod(sum, p))}"
+print(out)
